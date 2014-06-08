5df543b1bb4903f61838e08f41da49aeaf27594a
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-06-08 22:45:18 +0200

    add destroyer template; rearrange getters

---
+++ lib/c3d/util/destroy.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+module Destroyer
+  extend self
+
+  def destroy blob_id
+    #todo
+  end
+end
--- lib/c3d/util/getter.rb
+++ lib/c3d/util/getter.rb
@@ -3,14 +3,14 @@
 module Getter
   extend self
 
-  def get puller, blob_id
-    @puller = puller
+  def get blob_id
     if blob_id[0..1] == '0x'
       blob_id = blob_id[2..-1]
     end
     btih    = blob_id[0..39]
     dn      = blob_id[40..-1]
     mag_link = "magnet:?xt=urn:btih:" + btih + "&dn=" + dn
-    @puller.create @tor_file
+    $puller.create mag_link
+    puts "[C3D-EPM::#{Time.now.strftime( "%F %T" )}] Getting >>\t\t" + mag_link
   end
 end
