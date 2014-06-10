f897dce464f7ecf4e9256b3ae2ad7141f2ec362a
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-06-10 20:36:24 +0200

    fix rolling up to parent

--- lib/c3d/actors/crawler.rb
+++ lib/c3d/actors/crawler.rb
@@ -9,14 +9,17 @@ class Crawler
     @parse = find_the_peak contract
     @purge = []
     TreeBuilder.new @parse, @purge
+    p @parse
+    p @purge
   end
 
   private
     def find_the_peak contract
       parent   = send_query contract, '0x14'
-      until parent = '0x'
+      until parent == '0x'
         child  = parent
-        parent = send_query contract, '0x14'
+        parent = send_query parent, '0x14'
+        break if child == parent
       end
       return [child]
     end
@@ -28,5 +31,5 @@ end
 
 if __FILE__==$0
   require '../../c3d.rb'
-  Crawler.new
+  Crawler.new '0x5ebcc2546960f26a54b684d0e9dbdc152476703e'
 end
