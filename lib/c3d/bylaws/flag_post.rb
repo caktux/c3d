bf1d86e4bb8ddc971599ff614b1bdd0e10d32a7a
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-06-12 00:33:05 +0200

    finish current bylaw transactions

---
+++ lib/c3d/bylaws/flag_post.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+class FlagPost
+
+  def initialize post_id, flag_post_bylaw, flag_list_top
+    if post_id
+      build_transaction post_id, flag_post_bylaw
+      get_values flag_list_top
+    end
+  end
+
+  private
+
+    def build_transaction post_id, flag_post_bylaw
+      data      = [ post_id ]
+      $eth.transact flag_post_bylaw, data
+    end
+
+    def get_values flag_list_top
+      sleep 0.1                # to make sure the client has received the tx and posted to state machine
+      post_position = $eth.get_storage_at flag_list_top, '0x18'
+      if post_position == post_id
+        return true
+      else
+        return false
+      end
+    end
+end
---
+++ lib/c3d/bylaws/up_vote_post.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+class UpVotePost
+
+  def initialize post_id, down_vote_bylaw
+    if post_id
+      build_transaction post_id, down_vote_bylaw
+    end
+  end
+
+  private
+
+    def build_transaction post_id, down_vote_bylaw
+      data      = [ 'downvote', post_id ]
+      $eth.transact down_vote_bylaw, data
+    end
+end
