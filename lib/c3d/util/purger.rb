a08d51eb66551b5b45374062b08253e49ec1bb7b
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-06-12 01:29:32 +0200

    simpilfy purger rescue sequence

--- lib/c3d/util/purger.rb
+++ lib/c3d/util/purger.rb
@@ -4,17 +4,13 @@ class Purger
   include Celluloid
 
   def initialize blob
-    btih = blob[2..41]
-    dn   = blob[42..-1]
     begin
+      btih = blob[2..41]
+      dn   = blob[42..-1]
       link = "magnet:?xt=urn:btih:" + btih + "&dn=" + dn
       currently_downloading = $puller.all
       downloading = currently_downloading.select{|t| t["name"] == dn}.first["id"]
-    rescue
-      downloading = false
-    end
-    $puller.destroy downloading if downloading
-    begin
+      $puller.destroy downloading if downloading
       blob_file = File.join(ENV['BLOBS_DIR'], dn)
       if File.exists? blob_file
         File.delete blob_file
