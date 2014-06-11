1327a2351817d4e10926116afa8c66f5bdb626c6
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-06-11 03:19:19 +0200

    fix tree parsing and the test suite

--- lib/c3d.rb
+++ lib/c3d.rb
@@ -41,18 +41,18 @@ if __FILE__==$0
   blob1             = File.read(test_file) + "\n#{Time.now}\n#{rand(100000)}"
   blob2             = File.read(test_file) + "\n\n#{Time.now}\n\n#{rand(100000)}"
   blob3             = File.read(test_file) + "\n\n\n#{Time.now}\n\n\n#{rand(100000)}"
-  create_topc_bylaw = '0xac87f593b892e9c25bfe5c8e085ecf80bb9b7a70'
-  create_thrd_bylaw = '0xcc58b5745edec3574025f35c30ffc44fe843637e'
-  create_post_bylaw = '0xf1c290bd72d37e14d3bc7efc6eee6143ac54e37a'
-  swarum_top        = '0xd28218256cdfc6f5b369e3e549a3f528ba8ff322'
+  create_topc_bylaw = '0x5edc1f1af43c4070bbbfacff737e0e0aaffe240f'
+  create_thrd_bylaw = '0x8ea4bea370aacb4404052b6f0fd245bcbe19fcf0'
+  create_post_bylaw = '0xe022d281ee266b9abb86220d03bb6e21300bcfe6'
+  swarum            = '0x2c9e291d8e2ce5524c4544920fc98d97bdbee824'
 
   if ARGV[0] == 'first'
-    topic             = CreateTopic.new  blob1, create_topc_bylaw, swarum_top
+    topic             = CreateTopic.new  blob1, create_topc_bylaw, swarum
     topic_id          = topic.topic_id
   end
 
   if ARGV[0] == 'second'
-    topic_id = '0x69d3a3c7f1818475a064e419a5267be42c6d98b6'
+    topic_id = '0xb36d77fd073e8359ea93c5fd81dab77d898382b6'
   end
 
   if ARGV[0] == 'first'
@@ -62,7 +62,7 @@ if __FILE__==$0
   end
 
   if ARGV[0] == 'second'
-    thread_id = '0x6dbe4c8f2fe525cc93d22f732f695c52f1f0a77a'
+    thread_id = '0xf755208c68f1edf994ba424b6184c9bb57bdeaee'
   end
 
   if ARGV[0] == 'first'
@@ -82,5 +82,5 @@ if __FILE__==$0
     Subscribe.new
   end
 
-  EyeOfZorax.subscribe swarum_top
+  EyeOfZorax.subscribe swarum
 end
